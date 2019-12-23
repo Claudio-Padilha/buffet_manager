0d70f5bf6958752d891a168c199960b4c9857922
padilhaClaudio <padilha86@gmail.com>
2019-12-22 21:05:14 -0400

    Venda view improved

--- BuffetManager/src/application/VendaController.java
+++ BuffetManager/src/application/VendaController.java
@@ -10,6 +10,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.DialogPane;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
@@ -23,9 +24,13 @@ public class VendaController implements Initializable {
 	
 	@FXML Button btnAdd;
 	
+	@FXML Button btnDelete;
+	
 	@FXML ListView <String> itensVenda;
 	
 	@FXML Button btnRegistrarVenda;
+	
+	@FXML DialogPane total;
 	
 	ObservableList <String> itensCarrinho = FXCollections.observableArrayList("");
 	
@@ -36,14 +41,32 @@ public class VendaController implements Initializable {
 		
 		comBoxCliente.getItems().addAll("Alex", "César", "Maria", "Brinque Feliz", "Roupa Legal");
 		
-		comBoxProduto.getItems().addAll("Executivo", "Fit", "Gourmet");
+		comBoxProduto.getItems().addAll("Executivo", "Fitness", "Gourmet");
+		
+		total.contentTextProperty().setValue("0.0");
 	}
 	
 	public void add(ActionEvent event) {
 		
 		if (this.comBoxProduto.getSelectionModel().getSelectedItem() != null && qtde.getText() != null) {	
-			String aux = this.comBoxProduto.getSelectionModel().getSelectedItem() + "	" + qtde.getText();
+			String aux = this.comBoxProduto.getSelectionModel().getSelectedItem() + "    " + qtde.getText();
 			itensVenda.getItems().add(aux);
+			
+			Double novoTotal = Double.parseDouble(total.contentTextProperty().getValue().replace("Total R$ ", "")) + Double.parseDouble(qtde.getText()) * 10.0;
+			total.contentTextProperty().setValue("Total R$ " + String.valueOf(novoTotal));
+		}
+	}
+	
+	public void delete(ActionEvent event) {
+		if (itensVenda.getSelectionModel().getSelectedItem() != null) {
+			String selectedItem = itensVenda.getSelectionModel().getSelectedItem();
+			
+			int index = selectedItem.lastIndexOf(" ");
+			Double diferenca = Double.parseDouble(selectedItem.substring(index + 1, selectedItem.length()));
+			Double novoTotal = Double.parseDouble(total.contentTextProperty().getValue().replace("Total R$ ", "")) - diferenca * 10;
+			total.contentTextProperty().setValue("Total R$ " + String.valueOf(novoTotal));
+			
+			itensVenda.getItems().remove(itensVenda.getSelectionModel().getSelectedIndex());
 		}
 	}
 }
